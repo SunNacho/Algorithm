1cad80d4682bb519c567ffa5292c78693d15bbbb
SunNacho <105419350+SunNacho@users.noreply.github.com>
2023-04-04 02:58:52 +0900

    [level 0] Title: 짝수의 합, Time: 3.80 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+  
+    for(i=1;i<=n;i++){
+        if(i%2===0){
+        answer = answer + i
+           console.log(answer)}
+    }
+   return answer
+}
